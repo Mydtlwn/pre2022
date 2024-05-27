d9c32d7847c00b906479d0a8a30509aecf29015c
Mydtlwn <74440554+Mydtlwn@users.noreply.github.com>
2024-05-27 12:25:07 +0800

    Create query.py

---
+++ query.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+​
+address1="http://cov-spectrum.org/explore/World/AllSamples/"
+Month=["Past2M/","Past6M/"]
+address2="variant?nextcladePangoLineage="
+List=["JN.1.4.3*","LB.1*"]
+aaMutationLabel="&aaMutations1=S%3A"
+aaMutation=["S31-","S31F","S31P","F59S","F59L","S60P","T572I","T573I","K478R","A475V","A475S","S680F","S680P","S704L"]
+address3="&nextcladePangoLineage1="
+baseline = "KP.2*"
+address4="&analysisMode=CompareToBaseline&&analysisMode=CompareToBaseline&"
+address5=address1+Month[0]
+address6=address2+List[0]
+address7=aaMutationLabel+aaMutation[0]
+print (address5+address6+address7)
+print (15)
+print (34)
+​
