2993db642e4ca37153628bd1ace47103fff5d50a
Mydtlwn <74440554+Mydtlwn@users.noreply.github.com>
2024-05-27 21:09:26 +0800

    Create query2.py

---
+++ query2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+address1 = "http://cov-spectrum.org/explore/World/AllSamples/ "
+MonthChoose = ["Past2M/","Past6M/"]
+address2 = "variant?nextcladePangoLineage="
+ListChoose = ["JN.1.4.3*","LB.1*"]
+aaMutationLabel = "&aaMutations1=S%3A"
+aaMutation = ["S31-","S31F","S31P","F59S","F59L","S60P","T572I","T573I","K478R","A475V","A475S","S680F","S680P","S704L"]
+baseline = "KP.2*"
+address3 = "&nextcladePangoLineage1="
+address4 = "&analysisMode=CompareToBaseline&&analysisMode=CompareToBaseline&"
+final_address = ""
+for month in MonthChoose:
+    for variant in ListChoose:
+        for mutation in aaMutation:
+            current_address = address1 + month + address2 + variant + aaMutationLabel + mutation + address3 + baseline + address4
+            final_address += current_address
+
+print(final_address)
